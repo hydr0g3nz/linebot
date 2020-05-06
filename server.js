8759e4d767fb6118e451bede5de3276fa457cda8
Hydr0g3n <renbowneos@gmail.com>
2020-05-06 18:40:43 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 
 const PORT = process.env.PORT || 4000
-
-app.get('/',function(req,res) {res.sendFile('index.html')})
+var path = require('path');
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 
 app.get('*', (req, res) => {
